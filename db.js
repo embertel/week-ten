4d99a0ee6ee7c03f4d041efaf0992b724b9d8752
Emily Bertelson <emily.bertelson@gmail.com>
2013-07-30 14:48:36 -0600

    more routes, more functionality

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 var mongoose = require('mongoose');
 
+var actionsSchema = mongoose.Schema({
+	action: String
+});
+
 var retroSchema = mongoose.Schema({
     title: String,
     team: String,
-    description: String
+    description: String,
+    actions: [actionsSchema]
 });
 
 var Retro = mongoose.model('Retro', retroSchema);
--- routes.js
+++ routes.js
@@ -1,36 +1,71 @@
 var express = require('express');
 var Retro = require('./db');
+var mongoose = require('mongoose');
 
 module.exports = function(app){
 
-	// route stuff:
 	app.use(express.bodyParser());
 
-	app.get('/', function(req, res){
-		var body = "Hello world. I've got Express installed.";
-		res.setHeader('Content-Type', 'text/plain');
-		res.setHeader('Content-Length', body.length);
-		res.end(body);
+	// retro submission form
+	app.get('/', function (req, res){
+		res.render('form.jade');
 	});
 
-	// When I send a GET to /retro, I want to see a JSON list of retros.
-	app.get('/retro', function(req, res){
-		Retro.find(function (err, retros) {
+	// view JSON list of all retros
+	app.get('/retro', function (req, res){
+		Retro.find(function (err, retros){
 		  	if (err) console.log('Error finding db contents.');
-		  	var body = retros;
-		  	res.json(200, body);
+		  	res.json(200, retros);
 		});
 	});
 
-	app.post('/retro', function(req, res){
+	// view single retro
+	app.get('/retro/:id', function (req, res){
+		Retro.findById(req.params.id, function (err, retro){
+			if(err) console.log('Error: failed to find retro id ' + req.params.id);
+			res.json(200, retro);
+		});
+	});
+
+	// update specified retro
+	app.patch('/retro/:id', function (req, res){
+		Retro.findById(req.params.id, function (err, retro){
+			if(err) console.log('Error: failed to find retro id ' + req.params.id);
+
+			if(req.body.title) retro.title = req.body.title;
+			if(req.body.team) retro.team = req.body.team;
+			if(req.body.description) retro.description = req.body.description;
+
+			retro.save(function (err, retro){
+				if(err) console.log('Error: something messed up while updating retro id' + req.params.id);
+				res.send(retro);
+			});
+		});
+	});
+
+	// submit new retro
+	app.post('/retro', function (req, res){
 		var retro = new Retro({
 			title: req.body.title, 
 			team: req.body.team,
 			description: req.body.description
 		});
-		retro.save(function (err, retro) {
-		  	if (err) console.log('Error saving to the database.');
-		  	console.log(retro);
+		retro.save(function (err, retro){
+		  	if(err) console.log('Error saving to the database.');
+		  	res.send(retro);
 		});
 	});
+
+	// POST to /retro/<id>/action adds retro action
+	app.post('/retro/:id/action', function (req, res){
+		Retro.findById(req.params.id, function (err, retro){
+			if(err) console.log('Error: failed to find retro id ' + req.params.id);
+			retro.actions.push({action: req.body.action});
+			retro.save(function (err, retro){
+				if(err) console.log('Error: something messed up while updating retro id' + req.params.id);
+				res.send(retro);
+			});
+		});
+	});
+
 };
